5a95d749292ff4af093f2aff75d7af57ddd492e6
Anshul Kanwar <me@anshulkanwar.com>
2024-01-10 19:56:26 +0530

    update gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="mt-10 md:mt-20 max-w-fit mx-auto px-5">
       <div>
         <div className="text-center space-y-5 mb-10 md:mb-20">
-          <span className="text-4xl font-bold bg-gradient-to-r from-green-500 via-emerald-500 to-teal-500 bg-clip-text text-transparent">
+          <span className="text-4xl font-bold bg-gradient-to-r from-green-500 via-lime-500 to-emerald-500 bg-clip-text text-transparent">
             Graphitti
           </span>
           <p className="text-lg font-medium text-zinc-500">
